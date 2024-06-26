170139e7ea94792ec95f5b0b13bd6fc41817957b
Akash Pawar <akashpawar202k@gmail.com>
2024-06-26 21:11:15 +0530

    first commit

---
+++ day-1/helloworld.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!");
+	var var1 string;
+	var var2 int;
+	var var3 bool;
+	
+	fmt.Println(var1);
+	fmt.Println(var2);
+	fmt.Println(var3);
+
+}
